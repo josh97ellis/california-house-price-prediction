6eca9e2610e26409095faf1548c68ee3fc2036fa
Josh Ellis <76076622+josh97ellis@users.noreply.github.com>
2022-09-05 20:17:16 -0500

    Add files via upload

---
+++ lib/testing.py
@@ -0,0 +1,6 @@
+"""
+Imports the data not used in training, i.e. the test set
+and will evaluate the results of the currently trained model
+that exists within the project directory.
+"""
+print('Still needs Created, nothing to show yet')
